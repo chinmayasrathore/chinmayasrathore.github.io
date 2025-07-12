0f9e0caaba261410306b3fe7340347c0301c14d3
chinmayasrathore <chinmayasrathore@outlook.com>
2025-07-12 15:34:43 +0000

    Deploying to gh-pages from @ chinmayasrathore/chinmayasrathore.github.io@83687265fdebe5ea1d0240cc632b33e852b2a0e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-research",
           title: "research",
           description: "some research publications",
