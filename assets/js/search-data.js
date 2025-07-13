9f5f7f07ff4ba99e434f9ee134416d5843e6afad
chinmayasrathore <chinmayasrathore@outlook.com>
2025-07-13 08:28:16 +0000

    Deploying to gh-pages from @ chinmayasrathore/chinmayasrathore.github.io@a5982c57b6f6cad95f944ec93254f1b86e81e983 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,7 +438,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-dips",
           title: 'DIPS',
-          description: "a free software to teach introductory digital remote sensing concepts",
+          description: "A free software to teach introductory digital remote sensing concepts",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
