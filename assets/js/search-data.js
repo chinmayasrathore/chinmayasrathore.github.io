6de61f716e00a78bcde60872e9cbb407f95631be
chinmayasrathore <chinmayasrathore@outlook.com>
2025-08-07 16:52:20 +0000

    Deploying to gh-pages from @ chinmayasrathore/chinmayasrathore.github.io@ce9e4470b3645973b84633054e76a9053579f3bc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,8 +416,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-dips",
-          title: 'DIPS',
+          section: "News",},{id: "projects-tech-demonstrator-gps-logger-android-app",
+          title: 'Tech Demonstrator - GPS Logger Android App',
           description: "A teach demonstrator teaching note assessing the accuracy of GPS Logger app",
           section: "Projects",handler: () => {
               window.location.href = "/projects/10_project/";
