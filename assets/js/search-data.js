b1a738adbff9a7b6944aad1f5167489da7dbde50
chinmayasrathore <chinmayasrathore@outlook.com>
2025-08-08 08:37:08 +0000

    Deploying to gh-pages from @ chinmayasrathore/chinmayasrathore.github.io@faeb141f577c4aefe5f57889b2e8aacd261160b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,7 +443,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-dips",
           title: 'DIPS',
-          description: "A free software to teach introductory digital remote sensing concepts",
+          description: "A free software for Windows developed by Prof. Chinmaya S Rathore to teach introductory digital remote sensing and image analysis concepts",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
